abc0368f933332a1fcde9c61f4bcfc08f851a622
carlosdesousa91 <carlos.desousa91@outlook.com>
2021-02-05 00:06:52 -0300

    Criando app registro de hora extra

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Documento(models.Model):
-    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.nome
+        return self.descricao
