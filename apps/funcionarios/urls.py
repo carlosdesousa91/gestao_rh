2d315aa654008bcd075b4999611ffe4d6f44734a
carlosdesousa91 <31635606+carlosdesousa91@users.noreply.github.com>
2021-03-16 12:18:18 -0300

    backup projeto

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import FuncionariosList, FuncionarioEdit, FuncionarioDelete, FuncionarioCreate
+from .views import FuncionariosList, FuncionarioEdit, FuncionarioDelete, FuncionarioCreate, relatorio_funcionarios, Pdf
 
 urlpatterns = [
     path('', FuncionariosList.as_view(), name='list_funcionarios'),
     path('novo/', FuncionarioCreate.as_view(), name='create_funcionario'),
     path('edit/<int:pk>/', FuncionarioEdit.as_view(), name='update_funcionarios'),
     path('delete/<int:pk>/', FuncionarioDelete.as_view(), name='delete_funcionario'),
+    path('relatorio_funcionarios', relatorio_funcionarios, name='relatorio_funcionarios'),
+    path('relatorio_funcionarios_html', Pdf.as_view(), name='relatorio_funcionarios_html'),
     ]
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import ListView, UpdateView, DeleteView, CreateView
+from django.views.generic import ListView, UpdateView, DeleteView, CreateView, View
 from .models import Funcionario
+import io
+from reportlab.pdfgen import canvas
+from django.template.loader import get_template
+from xhtml2pdf import pisa
 
 
 class FuncionariosList(ListView):
@@ -35,3 +39,61 @@ class FuncionarioCreate(CreateView):
         funcionario.user = User.objects.create(username=username)
         funcionario.save()
         return super(FuncionarioCreate, self).form_valid(form)
+
+
+def relatorio_funcionarios(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="mypdf.pdf"'
+    
+    buffer = io.BytesIO()
+    p = canvas.Canvas(buffer)
+
+    p.drawString(10, 810, 'Relatório de funcionarios')
+
+    funcionarios = Funcionario.objects.filter(empresa=request.user.funcionario.empresa)
+
+    str_ = 'Nome: %s | Hora Extra %.2f'
+
+    y = 790
+    for funcionario in funcionarios:
+        p.drawString(10, y, str_ % (funcionario.nome, funcionario.total_horas_extra))
+        y -= 40
+
+    # Close the PDF object cleanly, and we're done.
+    p.showPage()
+    p.save()
+
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+
+    # FileResponse sets the Content-Disposition header so that browsers
+    # present the option to save the file.
+    #buffer.seek(0)
+    return response
+
+class Render:
+  @staticmethod
+  def render(path:str, params: dict, filename:str):
+    template = get_template(path)
+    html = template.render(params)
+    response = io.BytesIO()
+    pdf = pisa.pisaDocument(
+      io.BytesIO(html.encode("UTF-8")), response)
+    if not pdf.err:
+      response = HttpResponse(
+        response.getvalue(), content_type='application/pdf')
+      response['Content-Disposition'] = 'attachment; filename=%s.pdf' % filename
+      return response
+    else:
+      return HttpResponse("Error Rendering PDF", status=400)
+
+class Pdf(View):
+
+  def get(self, request):
+    params = {
+      'today': 'Variavel today',
+      'sales': 'Variavel sales',
+      'request': request,
+    }
+    return Render.render('funcionarios/relatorio.html', params, 'myfile')
