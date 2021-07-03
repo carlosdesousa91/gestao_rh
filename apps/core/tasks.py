9ca89448092a127d16228968146f3e064174f0b4
Carlos Araujo de Sousa <carlos.sousa@terceiro.rnp.br>
2021-07-03 13:56:06 -0300

    configurando celery

--- apps/core/tasks.py
+++ apps/core/tasks.py
@@ -1,7 +1,7 @@
 # Create your tasks here
 
 from celery import shared_task
-from apps.core.models import Widget
+#from apps.core.models import Widget
 
 
 @shared_task
@@ -19,13 +19,13 @@ def xsum(numbers):
     return sum(numbers)
 
 
-@shared_task
-def count_widgets():
-    return Widget.objects.count()
+#@shared_task
+#def count_widgets():
+#   return Widget.objects.count()
 
 
-@shared_task
-def rename_widget(widget_id, name):
-    w = Widget.objects.get(id=widget_id)
-    w.name = name
-    w.save()
+#@shared_task
+#def rename_widget(widget_id, name):
+#    w = Widget.objects.get(id=widget_id)
+#    w.name = name
+#    w.save()
